bf5a64a42d76f0e7e44492fe2278c2e949880ffe
Unknown <t8pito01@students.oamk.fi>
2018-09-26 18:17:33 +0300

    lisätty eka php tiedosto

---
+++ second.php
@@ -0,0 +1,15 @@
+<h2>Second Page</h2>
+		<p>
+			This is the second page.
+		</p>
+
+		<h3> PHP variable </h3>
+
+		<?php
+
+		$name='Garfield';
+		$age=5;
+		echo 'Name of the cat is: ' .$name  'and it is' .age 'years old'
+
+
+		?>
